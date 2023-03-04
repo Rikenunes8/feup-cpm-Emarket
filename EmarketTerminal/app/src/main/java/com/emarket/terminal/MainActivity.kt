b57494ebe5f303b3a542b0ed7a0d5624d3123d7a
Henrique Nunes <hrn2001@gmail.com>
2023-03-04 19:54:08 +0000

    Read qr code and display its content

--- EmarketTerminal/app/src/main/java/com/emarket/terminal/MainActivity.kt
+++ EmarketTerminal/app/src/main/java/com/emarket/terminal/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.emarket.terminal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_CAMERA_PERMISSION = 1
+        private const val REQUEST_CODE_QR_SCAN = 49374
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.mn_scanner -> {
+                if (!requestCameraPermission())
+                    read.launch(IntentIntegrator(this).createScanIntent())
+            }
+        }
+        return true
+    }
+
+    private fun requestCameraPermission(): Boolean {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (permission == PackageManager.PERMISSION_GRANTED) return false
+        val requests = arrayOf(Manifest.permission.CAMERA)
+        ActivityCompat.requestPermissions(this, requests, REQUEST_CAMERA_PERMISSION)
+        return true
+    }
+
+    private val read = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val intentResult : IntentResult? = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+        if (intentResult != null) {
+            if (intentResult.contents != null) {
+                Log.d(TAG, "QR Code Content: ${intentResult.contents}")
+                Toast.makeText(this, intentResult.contents, Toast.LENGTH_LONG).show()
+                processQRCode(intentResult)
+            } else {
+                Log.d(TAG, "Scan failed")
+            }
+        }
+    }
+
+    private fun processQRCode(result : IntentResult) {
+        // TODO do something useful
+        findViewById<TextView>(R.id.qr_value).text = result.contents
     }
 }
