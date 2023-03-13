3a7cee2741165880f34ecfdd9c95065b1a3caa10
carolmcca <carolina.albuquerque.cmp@gmail.com>
2023-03-13 11:26:23 +0000

    Create Shopping page layout - basket items + action on bar

--- EmarketCustomer/app/src/main/java/com/emarket/customer/RegisterActivity.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.emarket.customer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val intent = Intent(this, ShoppingActivity::class.java)
+        startActivity(intent)
     }
 }
