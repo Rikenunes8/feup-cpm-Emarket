47a817ae4170eb003dd3d0ea389f50d3a020db21
carolmcca <carolina.albuquerque.cmp@gmail.com>
2023-03-14 19:08:41 +0000

    Add total value on bottom bar

--- EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import java.io.Console
 
 val product1 = Product(R.drawable.icon, "Apple", 3.0, 1, 3.0)
 val product2 = Product(R.drawable.icon, "Banana", 4.0, 3, 12.0)
@@ -21,6 +22,11 @@ class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val totalView by lazy {findViewById<TextView>(R.id.total_price)}
+        var sum = 0.0
+        items.forEach{sum += it.price}
+        totalView.text = "$sum€"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
