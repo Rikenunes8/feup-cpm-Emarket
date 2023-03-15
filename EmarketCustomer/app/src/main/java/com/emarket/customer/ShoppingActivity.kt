ed97d9e91093ef299ec7c8046669f32df6fb84fd
Henrique Nunes <hrn2001@gmail.com>
2023-03-15 18:11:38 +0000

    Use recycler view to landscape and portrait

--- EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
@@ -1,18 +1,17 @@
 package com.emarket.customer
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
-import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
-import android.view.Menu
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.io.Console
+import com.emarket.customer.model.Product
 
 val product1 = Product(R.drawable.icon, "Apple", 3.0, 1, 3.0)
 val product2 = Product(R.drawable.icon, "Banana", 4.0, 3, 12.0)
@@ -20,33 +19,61 @@ val product2 = Product(R.drawable.icon, "Banana", 4.0, 3, 12.0)
 
 class ShoppingActivity : AppCompatActivity() {
 
-    private val items : ArrayList<Product> = arrayListOf(product1, product2, product1, product1, product1, product1, product1, product1)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val totalView by lazy {findViewById<TextView>(R.id.total_price)}
-        var sum = 0.0
-        items.forEach{sum += it.price}
-        totalView.text = "$sum€"
+        val rv = findViewById<RecyclerView>(R.id.rv_basket)
+
+        val orientation = if (Configuration.ORIENTATION_PORTRAIT == resources.configuration.orientation) RecyclerView.VERTICAL else RecyclerView.HORIZONTAL
+        rv.layoutManager = LinearLayoutManager(this, orientation, false)
+        rv.adapter = BasketAdapter()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
 
-    override fun onStart() {
-        super.onStart()
-        var layout = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            R.layout.basket_list_item
-        } else {
-            R.layout.basket_gallery_item
+}
+
+class BasketAdapter : RecyclerView.Adapter<BasketAdapter.ProductItem>() {
+
+    private val productItems : Array<Product> = arrayOf(product1, product2, product1, product2, product1, product2, product1, product2, product1, product2)
+
+
+    class ProductItem(val item: View) :  RecyclerView.ViewHolder(item) {
+        private val icon: ImageView = item.findViewById(R.id.item_icon)
+        private val name: TextView = item.findViewById(R.id.item_name)
+        private val price: TextView = item.findViewById(R.id.item_price)
+        private val qnt: TextView = item.findViewById(R.id.item_qnt)
+        private val total: TextView = item.findViewById(R.id.item_total_price)
+
+
+        fun bindData(product: Product) {
+            icon.setImageResource(product.imgRes)
+            name.text = product.name
+            println(product.price)
+            price.text = "Price: ${product.price} €"
+            qnt.text = "${product.qnt} x"
+            total.text = "${product.total} €"
         }
-        val basketView by lazy { findViewById<RecyclerView>(R.id.basket_listview) }
-        basketView.adapter = (BasketAdapter(this@ShoppingActivity, items, layout))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, vType: Int): ProductItem {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.basket_item, parent, false)
+        return ProductItem(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductItem, pos: Int) {
+        holder.bindData(productItems[pos])
+    }
+
+    override fun getItemCount(): Int {
+        return productItems.size
+    }
+
+    fun getItem(pos: Int): Product {
+        return productItems[pos]
+    }
+
+    fun addProductItem(product : Product) {
 
     }
 }
---
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/model/Product.kt
@@ -0,0 +1,10 @@
+package com.emarket.customer.model
+
+data class Product (
+    var imgRes: Int,
+    var name: String,
+    var price: Double,
+    var qnt: Int,
+    var total: Double
+)
+
