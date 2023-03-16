c3e4f2a9edb07ef2c08837b1888a348dd2982153
carolmcca <carolina.albuquerque.cmp@gmail.com>
2023-03-16 10:47:22 +0000

    Add and remove shopping items

--- EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/ShoppingActivity.kt
@@ -1,25 +1,27 @@
 package com.emarket.customer
 
-import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.emarket.customer.model.Product
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 val product1 = Product(R.drawable.icon, "Apple", 3.0, 1, 3.0)
 val product2 = Product(R.drawable.icon, "Banana", 4.0, 3, 12.0)
-private val productItems : Array<Product> = arrayOf(product1, product2, product1, product2, product1, product2, product1, product2, product1, product2)
+private val productItems : MutableList<Product> = mutableListOf(product1, product2, product1, product2, product1, product2, product1, product2, product1, product2)
 
 class ShoppingActivity : AppCompatActivity() {
-
+    var adapter = BasketAdapter(productItems)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
@@ -28,12 +30,19 @@ class ShoppingActivity : AppCompatActivity() {
 
         val orientation = if (Configuration.ORIENTATION_PORTRAIT == resources.configuration.orientation) RecyclerView.VERTICAL else RecyclerView.HORIZONTAL
         rv.layoutManager = LinearLayoutManager(this, orientation, false)
-        rv.adapter = BasketAdapter(productItems)
+        rv.adapter = adapter
 
         val totalView by lazy {findViewById<TextView>(R.id.total_price)}
         var sum = 0.0
         productItems.forEach{sum += it.price}
         totalView.text = "$sum€"
+BasketAdapter(productItems)
+        val addBtn by lazy {findViewById<FloatingActionButton>(R.id.add_item)}
+        addBtn.setOnClickListener{
+            productItems.add(Product(R.drawable.icon, "new", 40.0, 1, 40.0))
+            adapter.notifyItemInserted(productItems.size-1)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,7 +53,7 @@ class ShoppingActivity : AppCompatActivity() {
 
 }
 
-class BasketAdapter(private val productItems : Array<Product>) : RecyclerView.Adapter<BasketAdapter.ProductItem>() {
+class BasketAdapter(private val productItems : MutableList<Product>) : RecyclerView.Adapter<BasketAdapter.ProductItem>() {
 
 
     class ProductItem(val item: View) :  RecyclerView.ViewHolder(item) {
@@ -53,12 +62,12 @@ class BasketAdapter(private val productItems : Array<Product>) : RecyclerView.Ad
         private val price: TextView = item.findViewById(R.id.item_price)
         private val qnt: TextView = item.findViewById(R.id.item_qnt)
         private val total: TextView = item.findViewById(R.id.item_total_price)
+        internal val delete: ImageButton = item.findViewById(R.id.delete_btn)
 
 
         fun bindData(product: Product) {
             icon.setImageResource(product.imgRes)
             name.text = product.name
-            println(product.price)
             price.text = "Price: ${product.price} €"
             qnt.text = "${product.qnt} x"
             total.text = "${product.total} €"
@@ -72,6 +81,12 @@ class BasketAdapter(private val productItems : Array<Product>) : RecyclerView.Ad
 
     override fun onBindViewHolder(holder: ProductItem, pos: Int) {
         holder.bindData(productItems[pos])
+
+        holder.delete.setOnClickListener(View.OnClickListener {
+            // remove your item from data base
+            productItems.removeAt(pos) // remove the item from list
+            notifyItemRemoved(pos) // notify the adapter about the removed item
+        })
     }
 
     override fun getItemCount(): Int {
