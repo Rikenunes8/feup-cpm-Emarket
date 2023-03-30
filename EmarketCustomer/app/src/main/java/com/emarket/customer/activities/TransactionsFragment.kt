4e448ff84f25246fc4987f418601981142178968
Rui Alves <ruialves20010323@gmail.com>
2023-03-30 19:19:09 +0100

    Create transactions fragment layout

--- EmarketCustomer/app/src/main/java/com/emarket/customer/activities/TransactionsFragment.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/activities/TransactionsFragment.kt
@@ -5,56 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.emarket.customer.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.emarket.customer.controllers.TransactionsListAdapter
+import com.emarket.customer.databinding.FragmentTransactionsBinding
+import com.emarket.customer.models.Product
+import com.emarket.customer.models.Transaction
+import com.emarket.customer.models.Voucher
 
 /**
- * A simple [Fragment] subclass.
  * Use the [TransactionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class TransactionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    // TODO remove this
+    private val productItems : MutableList<Product> = mutableListOf(product1, product2)
+
+    // TODO: Receive transactions from other side
+    private var transactions = mutableListOf(
+        Transaction(productItems, 0.0, null, 0.0, "2021-01-01"),
+        Transaction(productItems, 2.37, Voucher("1", 15), 13.32, "2023-03-30"),
+    )
+    private lateinit var binding: FragmentTransactionsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transactions, container, false)
+    ): View {
+        binding = FragmentTransactionsBinding.inflate(layoutInflater)
+
+        binding.rvTransactions.apply {
+            layoutManager = LinearLayoutManager(requireActivity(), RecyclerView.VERTICAL, false)
+            adapter = TransactionsListAdapter(transactions)
+        }
+
+        return binding.root
     }
 
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment TransactionsFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TransactionsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            TransactionsFragment()
     }
 }
--- EmarketCustomer/app/src/main/java/com/emarket/customer/models/Transaction.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/models/Transaction.kt
@@ -4,6 +4,6 @@ data class Transaction (
     var products : MutableList<Product> = mutableListOf(),
     var discounted : Double = 0.0,
     var voucher : Voucher? = null,
-    var total : Double = 0.0, // after discount
+    var total : Double = 0.0,
     var date : String? = null
 )
