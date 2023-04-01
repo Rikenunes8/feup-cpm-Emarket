08eae3e96941630c75cc9bd7c5e5ddb6668db7d6
carolmcca <carolina.albuquerque.cmp@gmail.com>
2023-04-01 12:59:01 +0100

    Improve landscape basket card

--- EmarketCustomer/app/src/main/java/com/emarket/customer/Constants.kt
+++ EmarketCustomer/app/src/main/java/com/emarket/customer/Constants.kt
@@ -17,7 +17,7 @@ object Constants {
     const val BASKET_ITEMS = "BASKET_ITEMS"
 
     // Server connection
-    const val SERVER_URL = "http://192.168.1.64:5000/"
+    const val SERVER_URL = "http://192.168.43.89:5000/"
     const val REGISTER_ENDPOINT = "register"
     const val TRANSACTIONS_ENDPOINT = "transactions"
     const val VOUCHERS_ENDPOINT = "vouchers"
